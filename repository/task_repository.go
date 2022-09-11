c45730167eacdfef9369ac9c7660b36c28cef998
Zakaria Wahyu <zakarianur6@gmail.com>
2022-09-12 00:09:12 +0700

    Fix update task

--- repository/task_repository.go
+++ repository/task_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"errors"
 	"github.com/zakariawahyu/go-gin-jwt-clean/entity"
 	"gorm.io/gorm"
 )
@@ -33,7 +34,7 @@ func (taskRepo *TaskRepositoryImpl) Update(task entity.Task) (entity.Task, error
 	result := taskRepo.db.Where("id = ? AND user_id = ?", task.ID, task.UserID).Updates(&task)
 
 	if result.RowsAffected == 0 {
-		return task, result.Error
+		return task, errors.New("You dont have access to update this task")
 	}
 
 	taskRepo.db.Preload("User").Find(&task)
